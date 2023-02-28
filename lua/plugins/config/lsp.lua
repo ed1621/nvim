04a305cbca574b8be0d442e7ee2b22a13e38bb28
Eduardo Martinez <eduardo-martinez@pluralsight.com>
2023-02-28 09:15:20 -0700

    Lsp stuff

--- lua/plugins/config/lsp.lua
+++ lua/plugins/config/lsp.lua
@@ -2,7 +2,7 @@ local mason_lspconfig = require("mason-lspconfig")
 
 mason_lspconfig.setup({
   ensure_installed = {
-    "sumneko_lua",
+    "lua_ls",
     "bashls",
     "dockerls",
     "jsonls",
